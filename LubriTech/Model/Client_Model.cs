c6d52a6a0e08529f72afca494c2494ac276b2ae1
JorgeLuis <jorgeluisu10@gmail.com>
2024-05-26 15:11:04 -0600

    Segundo intento

---
+++ LubriTech/Model/Client_Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LubriTech.Model
+{
+    internal class Client_Model
+    {
+    }
+}
