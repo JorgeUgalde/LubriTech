500eb96830033e31e8c237e69efd7a954f3f72f1
Luis Ca <crluiscarlos@hotmail.com>
2024-05-27 22:21:30 -0600

    Cambios en el Client Model

--- LubriTech/Model/Client/Client.cs
+++ LubriTech/Model/Client/Client.cs
@@ -8,16 +8,17 @@ namespace LubriTech.Model.Client
 {
     internal class Client
     {
-        public string Identification { get; set; }
+        public string Id { get; set; }
 
         public string FullName { get; set; }
 
-        public int MainPhoneNumber { get; set; }
+        public int MainPhoneNum { get; set; }
 
-        public int AdditionalPhoneNumber { get; set; }
+        public int AdditionalPhoneNum { get; set; }
 
         public string Email { get; set; }
 
         public string Address { get; set; }
+
     }
 }//End Client Class
--- LubriTech/Model/Client/Client_Model.cs
+++ LubriTech/Model/Client/Client_Model.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LubriTech.Model.Client
 {
-    internal class Client_Model
+    public class Client_Model
     {
+        public List<Client> loadAllClients()
+        {
+            List<Client> client = new List<Client>();
 
+            try
+            {
+                String selectQuery = "select * from Cliente";
+
+                DataTable tblCliente = new DataTable();
+                SqlDataAdapter adp = new SqlDataAdapter();
+                adp.SelectCommand = new SqlCommand(selectQuery /* , agregar la conexion*/);
+                foreach (DataRow dr in tblCliente.Rows)
+                {
+                    client.Add(new Client((string)dr[""]);
+                }
+                return client;
+            }
+            catch (Exception ex)
+            {
+                throw(ex);
+            }
+        }
     }
 }
