0fbf649405d4f8a4c71a0aad801d5728999a5fc6
marcellev <marcell.elizondo.@ucrso.info>
2024-05-28 21:13:54 -0600

    Creación clase vehiculo

---
+++ LubriTech/Model/Vehicle/Vehicle_Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LubriTech.Model
+{
+    public class Vehicle_Model
+    {
+        SqlConnection conn = new SqlConnection(LubriTech.Properties.Settings.Default.connString);
+
+        public List<Vehicle> loadAllVehicles()
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            try
+            {
+                conn.Open();
+                String selectQuery = "SELECT * FROM Vehiculo";
+                SqlCommand cmd = new SqlCommand(selectQuery, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    vehicles.Add(new Vehicle(reader["Placa"].ToString(), reader["TipoMotor"].ToString(), Convert.ToDouble(reader["Kilometraje"]), reader["Marca"].ToString(), reader["Modelo"].ToString(), Convert.ToInt32(reader["Anio"]), reader["Transmision"].ToString(), reader["IdentificacionCliente"].ToString()));
+                }
+                return vehicles;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
--- LubriTech/Program.cs
+++ LubriTech/Program.cs
@@ -9,15 +9,12 @@ namespace LubriTech
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Products());
+            Application.Run(new Vehicles());
         }
     }
 }
