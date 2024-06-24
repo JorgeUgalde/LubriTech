34a38818f6eff4f170f29105af429b1a4f7d0553
marcellev <marcell.elizondo.@ucrso.info>
2024-06-23 23:53:11 -0600

    Creación TipoMotor, quedó funcionando bien

---
+++ LubriTech/Controller/Engine_Controller.cs
@@ -0,0 +1,49 @@
+﻿using LubriTech.Model;
+using LubriTech.Model.Vehicle_Information;
+using LubriTech.Model.Client_Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LubriTech.Controller
+{
+    /// <summary>
+    /// Controller class that manages business logic regarding engine type of vehicles.
+    /// It uses the class model <see cref="Engine_Model"/> to obtain useful methods.
+    /// </summary>
+    public class Engine_Controller
+    {
+        Engine_Model engineModel = new Engine_Model();
+
+        /// <summary>
+        /// Retrieves all engine types.
+        /// </summary>
+        /// <returns>A list of all engine types.</returns>
+        public List<Engine> getAll()
+        {
+            return engineModel.loadAllEngines();
+        }
+
+        /// <summary>
+        /// Inserts or updates a engine type.
+        /// </summary>
+        /// <param name="engine">engine object to upsert.</param>
+        /// <returns>True if the operation was successful; otherwise, false.</returns>
+        public Boolean upsert(Engine engine)
+        {
+            return engineModel.Upsert(engine);
+        }
+
+        /// <summary>
+        /// Retrieves a engine type by its identification.
+        /// </summary>
+        /// <param name="engineId">Identification of the engine type.</param>
+        /// <returns>The engine object.</returns>
+        public Engine getEngine(int engineId)
+        {
+            return engineModel.getEngine(engineId);
+        }
+    }
+}
---
+++ LubriTech/Model/Vehicle_Information/Engine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LubriTech.Model.Vehicle_Information
+{
+    /// <summary>
+    /// Representa el tipo de motor del carro.
+    /// </summary>
+    public class Engine
+    {
+        /// <summary>
+        /// Identificador único del tipo de motor.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre del tipo de motor.
+        /// </summary>
+        public string EngineType { get; set; }
+
+        /// <summary>
+        /// Estado del tipo de motor (activo o inactivo).
+        /// </summary>
+        public string State { get; set; }
+
+        public Engine() { }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="Engine"/> con los valores especificados.
+        /// </summary>
+        /// <param name="id">El Identificador del tipo de motor.</param>
+        /// <param name="name">El nombre del tipo de motor.</param>
+        /// <param name="state">El estado del tipo de motor.</param>
+        public Engine(int id, string engineType, string state)
+        {
+            this.EngineType = engineType;
+            this.Id = id;
+            this.State = state;
+        }
+
+        /// <summary>
+        /// Devuelve una cadena que representa el objeto actual.
+        /// </summary>
+        /// <returns>Una cadena que representa el tipo de motor.</returns>
+        public override string ToString()
+        {
+            return EngineType;
+        }
+    }
+}
