8f87dc80733f8a74e9f2201c9a76f05ec3a1a178
JorgeLuis <jorgeluisu10@gmail.com>
2024-06-24 17:37:31 -0600

    Inicio Citas funcional

---
+++ LubriTech/Model/Appointment_Information/Appointment.cs
@@ -0,0 +1,79 @@
+﻿using LubriTech.Model.Branch_Information;
+using LubriTech.Model.Client_Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LubriTech.Model.Appointment_Information
+{
+
+    /// <summary>
+    /// Clase que representa las citas del sistema
+    /// </summary>
+    public  class Appointment
+    {
+        /// <summary>
+        /// Identificador único de la cita.
+        /// </summary>
+        public int AppointmentID { get; set; }
+
+        /// <summary>
+        ///  Objeto cliente al que pertenece la cita.
+        /// </summary>
+        public Client client { get; set; }
+
+        /// <summary>
+        /// Fecha y hora de la cita
+        /// </summary>
+        public DateTime AppointmentDate { get; set; }
+
+        /// <summary>
+        /// Estado de la cita
+        /// </summary>
+        public short State { get; set; }
+
+        /// <summary>
+        /// Sucursal de la cita
+        /// </summary>
+        public Branch branch { get; set; }
+
+        /// <summary>
+        /// Constructor completo de la clase Appointment.
+        /// <summary>
+        /// <param name="appointmentDate"/>Fecha de la cita.</param>
+        /// <param name="appointmentID">Identificador único de la cita.</param>
+        /// <param name="client"/>   Cliente de la cita.</param>
+        /// <param name="state">Estado de la cita.</param>
+        /// <param name="branch">Sucursal de la cita.</param>
+
+
+        public Appointment (int appointmentID, Client client, DateTime appointmentDate, short state, Branch branch)
+        {
+           this.AppointmentID = appointmentID;
+            this.client = client;
+            this.AppointmentDate = appointmentDate;
+            this.State = state;
+            this.branch = branch;
+        }
+
+
+        /// <summary>
+        /// Constructor vacio de la clase Appointment.
+        /// </summary>
+        public Appointment()
+        {
+        }
+
+
+        /// <summary>
+        /// Sobrescribe el método ToString para mostrar al cliente de la cita.
+        /// </summary>
+        /// <returns>Nombre completo del cliente.</returns>
+        public override string ToString()
+        {
+            return client.ToString();
+        }
+    }
+}
---
+++ LubriTech/Model/Appointment_Information/Appointment_Model.cs
@@ -0,0 +1,151 @@
+﻿using LubriTech.Model.Branch_Information;
+using LubriTech.Model.Client_Information;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LubriTech.Model.Appointment_Information
+{
+    /// <summary>
+    /// Clase que maneja las operaciones relacionadas con la entidad <see cref="Appointment"/> en la base de datos.
+    /// </summary>
+    public class Appointment_Model
+    {
+        SqlConnection conn = new SqlConnection(LubriTech.Properties.Settings.Default.connString);
+
+
+        /// <summary>
+        /// Carga todas las citas de un día específico.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns>Lista de objetos Cita cargados desde la base de datos</returns>
+        public List<Appointment> loadDayAppointments(DateTime date)
+        {
+            List<Appointment> appointments = new List<Appointment>();
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Cita WHERE FechaHora = @date";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@date", date);
+                DataTable tblAppointments = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(tblAppointments);
+
+                foreach (DataRow row in tblAppointments.Rows)
+                {
+                    appointments.Add(new Appointment
+                    (
+                        Convert.ToInt32(row["Identificacion"]),
+                        new Client_Model().getClient(row["IdentificacionCliente"].ToString()),
+                        Convert.ToDateTime(row["FechaHora"]),
+                        Convert.ToInt16(row["Estado"]),
+                        new Branch_Model().GetBranch(Convert.ToInt32(row["IdentificacionSucursal"].ToString())) )
+                    );
+
+             
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener las citas");
+                throw ex;
+            }
+            finally { conn.Close(); }
+            return appointments;
+
+        }
+
+        /// <summary>
+        /// Busca una cita en especifico
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Retorna una cita  </returns>
+        public Appointment GetAppointment(int id)
+        {
+            Appointment appointment = null;
+            try
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM Cita WHERE Identificacion = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                DataTable tblAppointments = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(tblAppointments);
+
+                foreach (DataRow row in tblAppointments.Rows)
+                {
+                    new Appointment
+                    (
+                        Convert.ToInt32(row["Identificacion"]),
+                        new Client_Model().getClient(row["IdentificacionCliente"].ToString()),
+                        Convert.ToDateTime(row["FechaHora"]),
+                        Convert.ToInt16(row["Estado"]),
+                        new Branch_Model().GetBranch(Convert.ToInt32(row["IdentificacionSucursal"].ToString()))
+                    );
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener la cita");
+                throw ex;
+            }
+            finally { conn.Close(); }
+            return appointment;
+        }
+
+
+        /// <summary>
+        ///  Inserta o actualiza una cita en la base de datos
+        /// </summary>
+        /// <param name="appointment"></param>
+        /// <returns> Retorna un verdadero si la accion se completo con exito, o falso si no se completo con exito</returns>
+        public bool UpSert(Appointment appointment)
+        {
+            try
+            {
+                string query = "INSERT INTO Cita (IdentificacionCliente, FechaHora, Estado, IdentificacionSucursal) VALUES ( @client, @date, @state, @branch)";
+                if (GetAppointment(appointment.AppointmentID) != null)
+                {
+                    query = "UPDATE Cita SET IdentificacionCliente = @client, FechaHora = @date, Estado = @state, IdentificacionSucursal = @branch WHERE Identificacion = @id";
+                }
+
+                
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", appointment.AppointmentID);
+                cmd.Parameters.AddWithValue("@client", appointment.client.Id);
+                cmd.Parameters.AddWithValue("@date", appointment.AppointmentDate);
+                cmd.Parameters.AddWithValue("@state", appointment.State);
+                cmd.Parameters.AddWithValue("@branch", appointment.branch.Id);
+
+                if (conn.State == ConnectionState.Closed) { conn.Open(); }
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar la cita");
+                throw ex;
+            }
+            finally { conn.Close(); }
+        }
+
+
+
+
+
+    }
+}
